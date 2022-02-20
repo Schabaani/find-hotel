a8f2a965be1b43ab89bcb74d7bbd0811b3cd986a
Amish <a.shabani@megagroup.biz>
2022-02-20 20:28:08 +0330

    add home

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import {RootStackParamList} from '../../navigations/router';
-import {Colors} from '../../styles';
+import Styles from '../../styles/components';
 
 interface IProps {
   navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>;
 }
 
 export default function Home({navigation}: IProps) {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+  return (
+    <SafeAreaView style={Styles.Home.container}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('RoomSelector');
+        }}
+        style={Styles.Home.button}>
+        <Text style={Styles.Home.buttonText}>Select guests & rooms</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.WHITE,
-  },
-});
