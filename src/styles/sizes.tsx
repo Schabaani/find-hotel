cbf8c9c108a14b31ec63e044b72feafe66d6433e
Amish <a.shabani@megagroup.biz>
2022-02-20 20:26:59 +0330

    add sizes

--- src/styles/sizes.tsx
+++ src/styles/sizes.tsx
@@ -1,11 +1,18 @@
+const margin = 8;
+const border = 4;
+const fontSize = 10;
+
 const Sizes = {
-  title: {
-    fontSize: 16,
-    fontWeight: '900',
-  },
-  normalTxt: {
-    fontSize: 12,
-  },
+  margin1X: margin,
+  margin2X: 2 * margin,
+  margin3X: 3 * margin,
+  margin4X: 4 * margin,
+  border1X: border,
+  border2X: 2 * border,
+  border3X: 3 * border,
+  fontSize1X: fontSize,
+  fontSize2X: 2 * fontSize,
+  fontSize3X: 3 * fontSize,
 };
 
 export default Sizes;
